73b254e0f55ea88a3d15bba73c07bbdcad8f574d
hrishiky <hrishik.yendluri@gmail.com>
2024-12-15 12:16:29 -0600

    w2p1

---
+++ solutions/w2p1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Your number: %d\n", a);
+    
+    return 0;
+}
